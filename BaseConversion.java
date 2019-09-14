1bde5483f26ce23d9980074f559f41fda56e93c1
Orlando Vargas <OrlandoV87@live.com>
2019-09-14 13:15:14 -0400

    Added method based on the ascii value

--- BaseConversion.java
+++ BaseConversion.java
@@ -3,4 +3,14 @@ class BaseConversion {
     public static void main(String[] args) {
 
     }
+
+    public static int value(char c) {
+        if (c >= '0' && c <= '9') {
+            return (int) c - '0';
+        } else if (c >= 'a' && c <= 'z') {
+            return (int) c - 'a';
+        } else {
+            return (int) c - 'A';
+        }
+    }
 }
